b8619155802c80dc27f9a886f1c10ed2c211ba65
Mona Al Fouani <48865755+monaaaf@users.noreply.github.com>
2023-11-05 01:28:46 +0200

    Create python-solution.py for "Greatest Common Divisor of Strings" problem in LeetCode 75 study plan

---
+++ easy/greatest-common-divisor-of-strings/python-solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            
+            return a
+
+        gcd_length = gcd(len(str1), len(str2))
+        
+        return str1[:gcd_length]
